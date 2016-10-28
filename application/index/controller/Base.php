efe032cce7e954758a09485d3508c7774b713981
xiasf <811800545@qq.com>
2016-10-28 19:29:09 +0800

    ok

--- application/index/controller/Base.php
+++ application/index/controller/Base.php
@@ -8,7 +8,6 @@ class Base extends \think\Controller
 {
     public function _initialize()
     {
-    	var_dump($_GET);exit;
         if (!is_login() && (trim($_SERVER['PATH_INFO'], '/') != 'index/index/api')) {
 			$this->error('请先登录哦！', 'User/login');
         }
