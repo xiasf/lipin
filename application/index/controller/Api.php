8fecc580792578478a5a616b2b4475d8bdd087f8
xiasf <811800545@qq.com>
2016-11-09 05:25:06 +0800

    修正接口.

--- application/index/controller/Api.php
+++ application/index/controller/Api.php
@@ -12,9 +12,9 @@ class Api extends \think\Controller
         $imei = $request->get('imei/s');
         $arr = Db::name('validation_log')->field('color,tagid,create_time,result')->where('imei', $imei)->order('id', 'desc')->limit(100)->select();
         foreach ($arr as &$value) {
-            $value['tagid'] = "认证ID：" . $value['tagid'];
-            $value['date'] = date('m月d', $value['create_time']);
-            $value['time'] = date('H:i', $value['create_time']);
+            $value['tagid'] = $value['tagid'];
+            $value['date']  = date('m月d', $value['create_time']);
+            $value['time']  = date('H:i', $value['create_time']);
         }
         return json($arr, 200, ['Cache-control' => 'no-cache,must-revalidate']);
     }
