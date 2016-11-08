158e924cbd2738533f60b54786142c4cf170c308
xiasf <811800545@qq.com>
2016-11-09 03:06:56 +0800

    .

--- application/index/controller/Api.php
+++ application/index/controller/Api.php
@@ -67,7 +67,7 @@ class Api extends \think\Controller
 
             if ($this->deviceCheck($getImei)) {
                 // 得到满足条件的批次
-                if ($getID && ($info = Db::name('tag')->where(['tagid' => $getID, 'status' => 1])->find())) {
+                if ($getID && ($info = Db::name('tag')->alias('t')->join('__INFO__ i')->where(['t.tagid' => $getID, 't.status' => 1])->find())) {
                     $arr = [
                         "video"   => $info['video'],
                         "pic"     => explode(',' ,$info['pic']),
